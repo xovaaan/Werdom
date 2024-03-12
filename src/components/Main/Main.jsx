29cae8e0f414de0bbc696aa3765515c62349e851
xovaaan <xovan089@gmail.com>
2024-03-12 20:10:55 +0600

    trx

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './Main.css';
 import { assets } from '../../assets/assets';
+import { Context } from '../../context/Context';
+
 
 const Main = () => {
+
+    const {onSent,recentPrompt,showResult,loading,resultData,setInput,input} = useContext(Context);
   return (
     <div className='main'>
         <div className="nav">
@@ -10,30 +14,65 @@ const Main = () => {
             <img src={assets.user_icon} alt="" />
         </div>
         <div className="main-container">
-            <div className="greet">
+            {!showResult
+            ?<>
+             <div className="greet">
                 <p><span>Hello, Sami</span></p>
                 <p>How can i help you today?</p>
             </div>
             <div className="cards">
                 <div className="card">
-                    <p>Suggest some thriller movies</p>
-                    <img src={assets.compass_icon_icon} alt="" />
-                </div>
-                <div className="card">
-                    <p>Suggest some thriller movies</p>
+                    <p>Suggest some interesting areas to travel</p>
                     <img src={assets.compass_icon} alt="" />
                 </div>
                 <div className="card">
-                    <p>Suggest some thriller movies</p>
-                    <img src={assets.compass_icon} alt="" />
+                    <p>Briefly explain this concept : Urban Planning</p>
+                    <img src={assets.bulb_icon} alt="" />
                 </div>
                 <div className="card">
-                    <p>Suggest some thriller movies</p>
-                    <img src={assets.compass_icon} alt="" />
+                    <p>Brainstorm team bonding activities for our work retreat</p>
+                    <img src={assets.message_icon} alt="" />
+                </div>
+                <div className="card">
+                    <p>Improve the readability of the following code</p>
+                    <img src={assets.code_icon} alt="" />
                 </div>
             </div>
+            </>:<div className='result'>
+                <div className="result-title">
+                    <img src={assets.user_icon} alt="" />
+                    <p>{recentPrompt}</p>
+                </div>
+                <div className="result-data">
+                    <img src={assets.gemini_icon} alt="" />
+                    {loading
+                    ?<div className='loader'>
+                         <hr />
+                         <hr />
+                         <hr />
+                    </div> :
+                     <p dangerouslySetInnerHTML={{__html:resultData}}></p>
+                    }
+                   
+                </div>
+
+            </div>
+        }
+           
+           <div className="main-bottom">
+            <div className="search-box">
+                <input onChange={(e)=>setInput(e.target.value)} value={input} type="text" placeholder='Write any prompt'/>
+                <div>
+                    <img src={assets.gallery_icon} alt="" />
+                    <img src={assets.mic_icon} alt="" />
+                    <img onClick={()=> onSent()} src={assets.send_icon} alt="" />
+                </div>
+            </div>
+            <p className='bottom-info'>
+                Werdom may display inaccurate info sometimes,including about people,so double check if there is any irrelevant info
+            </p>
+           </div>
         </div>
-      
     </div>
   )
 }
