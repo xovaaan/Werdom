4473fd38a9007e05c5117b96f4f3d24570d38b32
xovaaan <xovan089@gmail.com>
2024-03-14 02:57:49 +0600

    trnsss

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,40 +1,55 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import "./Sidebar.css";
 import { assets } from "../../assets/assets";
+import { Context } from "../../context/Context";
 
 const Sidebar = () => {
   const [extended, setExtended] = useState(false);
+  const { onSent, prevPrompts, setRecentPrompts,newChat } = useContext(Context);
+  const loadPrompt = async (prompt) => {
+   setRecentPrompts(prompt)
+    await onSent(prompt)
+  }
 
   return (
     <div className="sidebar">
       <div className="top">
-        <img onClick={()=>setExtended(prev=>!prev)} className="menu" src={assets.menu_icon} alt="Menu" />
-        <div className="new-chat">
+        <img
+          onClick={() => setExtended((prev) => !prev)}
+          className="menu"
+          src={assets.menu_icon}
+          alt="Menu"
+        />
+        <div onClick={()=>newChat()} className="new-chat">
           <img src={assets.plus_icon} alt="New-Chat" />
           {extended ? <p>New Chat</p> : null}
         </div>
         {extended ? (
           <div className="recent">
             <p className="recent-title">Recent</p>
-            <div className="recent-entry">
-              <img src={assets.message_icon} alt="" />
-              <p>What is React.....</p>
-            </div>
+            {prevPrompts.map((item, index) => {
+              return (
+                <div onClick={()=> loadPrompt(item)} className="recent-entry">
+                  <img src={assets.message_icon} alt="" />
+                  <p>{item.slice(0,18)}...</p>
+                </div>
+              )
+            })}
           </div>
         ) : null}
       </div>
       <div className="bottom">
         <div className="bottom-item recent-entry">
           <img src={assets.question_icon} alt="" />
-         {extended? <p>Help</p>:null} 
+          {extended ? <p>Help</p> : null}
         </div>
         <div className="bottom-item recent-entry">
           <img src={assets.history_icon} alt="" />
-         {extended?<p>History</p>:null} 
+          {extended ? <p>History</p> : null}
         </div>
         <div className="bottom-item recent-entry">
           <img src={assets.setting_icon} alt="" />
-          {extended?<p>Settings</p>:null} 
+          {extended ? <p>Settings</p> : null}
         </div>
       </div>
     </div>
